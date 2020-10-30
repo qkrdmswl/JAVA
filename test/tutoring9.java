5b7b13d3a13c7debd66b5d2c29b4403452254f74
qkrdmswl <eunji303@naver.com>
2020-10-30 23:13:12 +0900

    modify

--- test/tutoring9.java
+++ test/tutoring9.java
@@ -5,7 +5,6 @@ public class tutoring9 {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		Vector<Integer> v = new Vector<Integer>();
-		Iterator<Integer> it = v.iterator();
 		int sum=0, average=0;
 		
 		while(true){
@@ -13,14 +12,16 @@ public class tutoring9 {
 			int rf = sc.nextInt();
 			if(rf==0) {break;}
 			v.add(rf);
+			System.out.println(v.size());
 			
 			for(int i=0; i<v.size(); i++) {
-				int n = it.next();
-				System.out.println(n);
+				System.out.print(v.get(i)+" ");
+				sum += v.get(i);			
 			}
-			
-//			System.out.println("ÇöÀç Æò±Õ : ");
-//			average = sum / v.size();		
+			System.out.println();
+			average = sum / v.size();
+			System.out.println("ÇöÀç Æò±Õ : "+average);
+			sum = 0;
 		}
 	}
 
